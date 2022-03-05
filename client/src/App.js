4b22111cf6ae2eb3f7b5bfcdbaaf15a795ed1a00
Emily Reese <emilyreese.ga@gmail.com>
2022-03-05 16:27:37 -0500

    I have refactored the clients side code to work with APollo/GraphQl.

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,24 @@
-import logo from './logo.svg';
+
 import React from 'react';
-import { Route, Router, Routes} from 'react-router-dom';
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+
 import './App.css';
+import Login from './components/Login';
+import Signup from './components/Signup'
 import SignupButton from './components/parts/SignupButton';
 import LoginButton from './components/parts/LoginButton';
-import Login from './components/Login';
-import Signup from './components/Signup;'
 import './tailwind.config';
+
+
 
 
 function App() {
   return (
-
-    <div className="App">
-      <Routes>
-        <LoginButton>
-          {/* <Route>
-            pathname: 
-
-          </Route> */}
-        </LoginButton>
-        <SignupButton />
-      </Routes>
-   
+    
+      <div className="App">
+          <LoginButton />
+          <SignupButton />
        </div>
+
   );
 }
 
