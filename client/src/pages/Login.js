28407922a303040648bf2f4e307cedebb9a75464
Emily Reese <emilyreese.ga@gmail.com>
2022-03-08 18:04:21 -0500

    Changing the mutation names in the signup/login.js files.

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
 import { useMutation } from '@apollo/client';
 import { Link } from 'react-router-dom';
-import { LOGIN } from '../utils/mutations';
+import { LOGIN_MUTATION } from '../utils/mutations';
 import Auth from '../utils/auth';
 
 export default function Login(props) {
   const [formState, setFormState] = useState({ email: '', password: '' });
-  const [login, { error }] = useMutation(LOGIN);
+  const [login, { error }] = useMutation(LOGIN_MUTATION);
 
   const handleFormSubmit = async (event) => {
     event.preventDefault();
