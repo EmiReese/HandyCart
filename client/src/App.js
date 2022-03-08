d17ad466f1c16643a5b05167ad5f39b0cadccaf7
Triston Golden <triston_golden@yahoo.com>
2022-03-08 11:53:01 -0500

    switched to list format

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,12 @@
-import React from "react";
-import List from "./Pages/List";
-import Header from "./Components/Header";
-// import Footer from "./Components/Footer";
-import Budget from "./Components/Budget";
-import ExpenseTotal from "./Components/ExpenseTotal";
-import Remaining from "./Components/Remaining";
+import "./App.css";
+import GroceryList from "./Components/GroceryList";
 
 function App() {
   return (
-    <div className="h-max bg-yellow-200">
-      <Header />
-      <List />
-      <div className="ml-20 columns-3 flex justify-around">
-        <Budget />
-        <ExpenseTotal />
-        <Remaining />
-      </div>
-
-      {/* <Footer /> */}
+    <div className="grocery-app">
+      <GroceryList />
     </div>
   );
 }
+
 export default App;
