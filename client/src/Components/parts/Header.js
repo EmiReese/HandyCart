783dac14f3f4e16536d868db99ad675539a9e851
Emily Reese <emilyreese.ga@gmail.com>
2022-03-08 21:27:15 -0500

    I am styling the header.

---
+++ client/src/Components/parts/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header () {
+    return (
+        <div className="headerContainer">
+            <h1 className="headerName text-blue-600 bg-white font-medium ">
+                HandyCart 
+            </h1>
+        </div>
+    );
+}
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import SignupButton from '../components/parts/SignupButton';
 import LoginButton from '../components/parts/LoginButton';
-
+import Header from '../components/parts/Header'
 
 
 export default function Home() {
     return(
     <div>
 
-  <SignupButton />
-  <LoginButton />
+      <Header />
+      
+      <SignupButton />
+      <LoginButton />
 
     </div>
     );
